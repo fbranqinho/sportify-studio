03f303208a2684ee41075f8c7b9b082493b0a04e
Francisco Branquinho <fr.branquinho@gmail.com>
2025-08-20 23:11:22 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/team/manager-team-view.tsx
+++ src/components/team/manager-team-view.tsx
@@ -14,6 +14,7 @@ import { Badge } from "@/components/ui/badge";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { Trash2, UserPlus, Users, Search, X, Footprints, Square, Goal, Save } from "lucide-react";
 import { Tooltip, TooltipProvider, TooltipContent, TooltipTrigger } from "../ui/tooltip";
+import { cn } from "@/lib/utils";
 
 
 interface EnrichedTeamPlayer {
