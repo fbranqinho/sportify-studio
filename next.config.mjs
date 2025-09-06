061bd2aa96aa4815280be67bb499eaf5d3767d8a
Francisco Branquinho <fr.branquinho@gmail.com>
2025-09-06 22:03:17 +0000

    FAILED TO INITIALIZE FIREBASE ADMIN SDK !! ONDE ESTA ESTE ERRO ?? MOSTRR

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
-import type {NextConfig} from 'next';
 
-const nextConfig: NextConfig = {
-  /* config options here */
+import 'dotenv/config';
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
